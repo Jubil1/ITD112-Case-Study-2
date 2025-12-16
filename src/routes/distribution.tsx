9aaee21eb8a9580e3b9a8f6f7e2244d01b8f818d
jubil1 <jubil.ventic@gmail.com>
2025-12-17 07:02:30 +0800

    latest

--- src/routes/distribution.tsx
+++ src/routes/distribution.tsx
@@ -94,10 +94,10 @@ function DistributionCharts() {
             <div className="h-96">
               <ResponsiveLine
                 data={countryDensityData}
-                margin={{ top: 50, right: 180, bottom: 70, left: 80 }}
-                xScale={{ type: "linear", min: 1960, max: 2040 }}
+                margin={{ top: 50, right: 200, bottom: 60, left: 80 }}
+                xScale={{ type: "linear", min: "auto", max: "auto" }}
                 yScale={{
-                  type: "linear",
+                  type: "linear", 
                   min: "auto",
                   max: "auto",
                 }}
